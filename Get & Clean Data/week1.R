ad1453498352992231ac50888ab0f946af47546e
nontapat sumalnop <nonimm75@hotmail.com>
2019-07-04 15:43:27 +0700

    finally

--- Get & Clean Data/week1.R
+++ Get & Clean Data/week1.R
@@ -13,4 +13,45 @@ dateDownloaded <- date() # keep track of the date u downloaded the file
 cameraData <- read.table('D:/datasciencecoursera/Get & Clean Data/camera.csv',sep = ',',header = TRUE)
 head(cameraData)
 
-# reading XML : extensible markup language
+# reading XML : extensible markup language : frequently used in web scraping
+library(XML)
+library(RCurl)
+URL <- 'https://www.w3schools.com/xml/simple.xml'
+
+xData <- getURL(URL)
+doc <- xmlParse(xData)
+
+rootNode <- xmlRoot(doc) # the wrap of whole thing
+xmlName(rootNode)
+
+names(rootNode) # 1 layer nested inside of root node
+
+# you can access it like a list using [[]] (note that [] will also come with $)
+rootNode[[1]]
+rootNode[[1]][[1]] # fist element of the first element of the xml list
+rootNode[[1]][[2]]
+
+# extract parts of the file
+x<-xmlSApply(rootNode,xmlValue) # get every value in the file // all value in every tags together
+y <- xmlSApply(rootNode[[1]],xmlValue)
+# if you want to extract more specific component, you have to learn XPATH (another language)
+xpathSApply(rootNode,'//name',xmlValue)
+xpathSApply(rootNode,'//price',xmlValue)
+
+# do web scraping : go to https://www.espn.com/nfl/team/_/name/bal/baltimore-ravens
+# right click >> view source
+
+
+# reading JSON : javascript object notation
+library(jsonlite)
+jsonData <- fromJSON('https://api.github.com/users/jtleek/repos') # gets structured dataframe
+names(jsonData)
+# try names(jsonData$) and you will see that $owner actually score another df
+names(jsonData$owner)
+jsonData$owner$login
+
+names(jsonData$license)
+
+# you can turn R dataframe into JSON df
+myjson <- toJSON(iris,pretty = T)
+cat(myjson)
